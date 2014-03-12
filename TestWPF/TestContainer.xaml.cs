8947a703588ab9750ee56e3635abf44911c7abda
thekingdom <korakotru@gmail.com>
2014-03-12 17:33:13 +0700

    Check-In

--- TestWPF/TestContainer.xaml.cs
+++ TestWPF/TestContainer.xaml.cs
@@ -19,7 +19,16 @@ namespace TestWPF
     /// </summary>
     public partial class TestContainer : Window
     {
-   
+        #region ============== Declaration ====================
+        int columnA = 0;
+        int numberOfObjects = 0;
+
+        double FirstXPos, FirstYPos, FirstArrowXPos, FirstArrowYPos;
+        object MovingObject;
+        Line Path1, Path2, Path3, Path4;
+        Rectangle FirstPosition, CurrentPosition;
+        #endregion
+
         public TestContainer()
         {
             InitializeComponent();
@@ -31,44 +40,79 @@ namespace TestWPF
             sv.Content = gridPanelA;
 
         }
-        int columnA = 0;
-        int numberOfObjects = 0;
+
+        private void RootWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
         private void btnSet_Click(object sender, RoutedEventArgs e)
         {
             columnA = int.Parse(txtColumnSizeA.Text.Trim());
             numberOfObjects = int.Parse(txtNumberOfObjects.Text.Trim());
 
+            // Create object and add to list
             List<Button> itemList = new List<Button>();
             for (int i = 0; i < numberOfObjects; i++)
             {
-                itemList.Add(new Button() { Name="item"+i });
+                itemList.Add(new Button() { Name = "item" + i });
             }
 
             // Set the layout 
-            BuildGrid(numberOfObjects, columnA, gridPanelA); 
+            BuildGrid(numberOfObjects, columnA, gridPanelA);
 
             int currentRow = 0, currentColumn = 0;
-            int totalObjects = itemList.Count; 
+            int totalObjects = itemList.Count;
             foreach (Button item in itemList)
-            {  
+            {
                 if (currentColumn >= columnA)
                 {
                     ++currentRow;
                     currentColumn = 0;
-                    if (totalObjects == 0 )
-                        break; 
+                    if (totalObjects == 0)
+                        break;
                 }
                 item.Margin = new Thickness(10);
-                item.Content = "[" + currentRow.ToString() + "]" + "[" + currentColumn.ToString() + "]"; 
+                item.Content = "[" + currentRow.ToString() + "]" + "[" + currentColumn.ToString() + "]";
                 Grid.SetRow(item, currentRow);
                 Grid.SetColumn(item, currentColumn);
                 gridPanelA.Children.Add(item);
 
-                currentColumn++; 
+                /*
+                Binding drag and drop event handler to control 
+                */
+                //item.PreviewMouseLeftButtonDown += item_PreviewMouseLeftButtonDown;
+                //item.PreviewMouseMove += item_PreviewMouseMove;
+                //item.PreviewMouseLeftButtonUp += item_PreviewMouseLeftButtonUp;
+                 
+                // Update counter for next loop
+                currentColumn++;
                 --totalObjects;
             }
-             
         }
+
+
+        #region ============= Drag and Drop event handler =============
+        void item_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        } 
+        void item_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed && sender == MovingObject)
+            {
+                (sender as Control).SetValue(LeftProperty, e.GetPosition((sender as Control).Parent as Control).X - FirstXPos - 20);
+                (sender as Control).SetValue(TopProperty, e.GetPosition((sender as Control).Parent as Control).Y - FirstYPos - 20);
+            }
+        } 
+        void item_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            FirstXPos = e.GetPosition(sender as Control ).X;
+            FirstYPos = e.GetPosition(sender as Control).Y;
+            FirstArrowXPos = e.GetPosition((sender as Control).Parent as Control).X - FirstXPos - 20 ;
+            FirstArrowYPos = e.GetPosition((sender as Control).Parent as Control).Y - FirstYPos - 20;
+        }
+        #endregion
 
         private void BuildGrid(int numberOfObjects, int column, Grid grid)
         {
@@ -76,7 +120,7 @@ namespace TestWPF
             grid.ColumnDefinitions.Clear();
             ColumnDefinition cd;
             RowDefinition rd;
-            int totalRow =  numberOfObjects/column  ;
+            int totalRow = numberOfObjects / column;
             double width = grid.Width / column;
             // Create column
             for (int i = 0; i < column; i++)
@@ -89,11 +133,13 @@ namespace TestWPF
             for (int i = 0; i < totalRow; i++)
             {
                 rd = new RowDefinition();
-                rd.Height = new GridLength( ( 50*width)/100  );
+                rd.Height = new GridLength((50 * width) / 100);
                 grid.RowDefinitions.Add(rd);
             }
 
         }
-      
+
+       
+
     }
 }
---
+++ TestWPF/TestForm.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestWPF
+{
+    /// <summary>
+    /// Interaction logic for TestForm.xaml
+    /// </summary>
+    public partial class TestForm : Window
+    {
+        double FirstXPos, FirstYPos, FirstArrowXPos, FirstArrowYPos;
+        object MovingObject;
+        Line Path1, Path2, Path3, Path4;
+        Rectangle FirstPosition, CurrentPosition;
+
+        public TestForm()
+        {
+            InitializeComponent();
+
+            // Binding Event
+            item.PreviewMouseLeftButtonDown += item_PreviewMouseLeftButtonDown;
+            item.PreviewMouseMove += item_PreviewMouseMove;
+            item.PreviewMouseLeftButtonUp += item_PreviewMouseLeftButtonUp;
+        }
+        #region ============= Drag and Drop event handler =============
+        void item_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        void item_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed && sender == MovingObject)
+            {
+                (sender as Control).SetValue(Canvas.LeftProperty, e.GetPosition((sender as Control).Parent as Control).X - FirstXPos - 20);
+                (sender as Control).SetValue(Canvas.TopProperty, e.GetPosition((sender as Control).Parent as Control).Y - FirstYPos - 20);
+            }
+        }
+        void item_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            FirstXPos = e.GetPosition(sender as Control).X;
+            FirstYPos = e.GetPosition(sender as Control).Y;
+            FirstArrowXPos = e.GetPosition((sender as Control).Parent as Control).X - FirstXPos - 20;
+            FirstArrowYPos = e.GetPosition((sender as Control).Parent as Control).Y - FirstYPos - 20;
+        }
+        #endregion
+    }
+}
