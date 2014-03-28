cb0c70b1c93a947eb98404fd2c5fa12986cc0724
thekingdom <korakotru@gmail.com>
2014-03-28 17:38:03 +0700

    ...

---
+++ TestWPF/Data/ItemModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWPF.Data
+{
+    public class ItemModel
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public string Description { get; set; }
+        public int Qty { get; set; }
+        public decimal Cost { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class ItemModelList : List<ItemModel> { }
+
+}
